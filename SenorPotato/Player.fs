6100e72f12ca8ef2bc9260833bbdd1bb8c985de9
htoma <horia.toma@availpro.com>
2017-03-16 16:00:22 +0100

    Initial version

---
+++ SenorPotato/Player.fs
@@ -0,0 +1,12 @@
+﻿namespace SenorPotato
+
+type Skills = { Defence: int
+                Attack: int
+                Penalty: int
+                Stamina: int }
+
+type Player = { Name: string
+                Description: string
+                Overall: int
+                Skills: Skills
+                Captain: Skills->Skills }
---
+++ SenorPotato/Script.fsx
@@ -0,0 +1,17 @@
+﻿#load "Player.fs"
+open SenorPotato
+
+let skills = { Defence=3
+               Attack=6
+               Penalty=5
+               Stamina=6 }
+
+let captainAttack (skills: Skills) =
+    {skills with Attack=skills.Attack+2}
+
+let player = { Name="Senor Potato"
+               Description="amazing player"
+               Overall=32
+               Skills=skills
+               Captain=captainAttack }
+
